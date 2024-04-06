9e0cfdad3a63e668a6aae778f5973932cafff277
Kwanda Madela <55926698+Developer-KT@users.noreply.github.com>
2024-04-06 17:25:55 +0200

    Update Navbar.jsx
    Add "bouncy nav" animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,61 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Navbar() {
+
+  useEffect(() => {
+    gsap.registerPlugin(Flip); //Let gsap know that we've added this plugin to our project
+
+    const links = document.querySelectorAll(".tab a");
+    const activeNav = document.querySelector(".active-nav");
+
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        //Turn navs blue
+        gsap.to(links, { color: "black" });
+
+        if (document.activeElement === link) {
+          gsap.to(link, { color: "#385ae0" });
+        }
+
+        console.log(document.activeElement);
+
+        //Move the line
+        const state = Flip.getState(activeNav);
+        link.appendChild(activeNav);
+        Flip.from(state, {
+          duration: 1.25,
+          absolute: true,
+          ease: "elastic.out(1,0.5)",
+        });
+      });
+    });
+  });
+
   return (
-    <header>
+    <header id="header">
       <img src="/images/HOME.CO.svg" alt="Home.Co logo" />
       <nav>
         <ul className="nav-tabs">
-          <li className="tab">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="tab">
-            <Link to="/shop-all">Shop All</Link>
-          </li>
-          <li className="tab">
-            <Link to="/specials">Specials</Link>
-          </li>
+          <div className="tab-div">
+            <li className="tab">
+              <Link to="/" id="home-btn">
+                Home
+              </Link>
+            </li>
+            <div className="active-nav"></div>
+          </div>
+          <div className="tab-div">
+            <li className="tab">
+              <Link to="/shop-all">Shop All</Link>
+            </li>
+          </div>
+          <div className="tab-div">
+            <li className="tab">
+              <Link to="/specials">Specials</Link>
+            </li>
+          </div>
         </ul>
       </nav>
       <button className="btn" id="contact-btn">
